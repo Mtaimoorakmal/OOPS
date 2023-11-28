759eb0f508a1eb0b3be170d974068e0af7841289
Mtaimoorakmal <94taimoor@gmail.com>
2023-11-29 00:25:40 +0500

    created and print a class with its instances

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("this is OOPS in [python]")
+# Creat a Class
+class Book:
+    def __init__(self , title):
+        self.title = title
+
+# Creat instances of the Class
+
+b1 = Book("brave new World")
+b2 = Book("War and Peace")
+
+
+
+# Print the Class and property 
+
+print(b1)
+print(b1.title)
+
